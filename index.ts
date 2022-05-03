1e89a3671207e466a356444ee26e373155743a19
Mike Aizatsky <mike.aizatsky@gmail.com>
2022-05-03 11:14:22 -0700

    modern export syntax

--- index.ts
+++ index.ts
@@ -4,6 +4,4 @@ export * from "./random.js";
 export * from "./server.js";
 export * from "./storage.js";
 
-export namespace d {
-    export * from "./decoder.js";
-};
+export * as d from "./decoder.js";
